761340fb7815d3e75126e083cc0f6290b4aae058
SQUIDzillaNFT <nrwisch@gmail.com>
2022-05-16 16:08:30 -0500

    truffle migration

--- Wallet/migrations/2_deploy_contract.js
+++ Wallet/migrations/2_deploy_contract.js
@@ -1,9 +1,9 @@
 const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 
-const Migrations = artifacts.require("Wallet");
+const Wallet = artifacts.require("Wallet");
 
-module.exports = async function (deployer, _network, accounts) {
-  await deployer.deploy(Wallet, [accounts[0]], [accounts[1]], [accounts[2]], 2);
+module.exports = async function(deployer, _network, accounts) {
+  await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
   const wallet = await Wallet.deployed();
   // truffle always give web3 instance - send 10k wei(eth) to contract for testing
   await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
