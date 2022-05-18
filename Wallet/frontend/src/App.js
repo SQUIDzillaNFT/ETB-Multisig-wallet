23ead02f7a578d7d28450eb07a849a02f53b4438
SQUIDzillaNFT <nrwisch@gmail.com>
2022-05-17 20:28:14 -0500

    header

--- Wallet/frontend/src/App.js
+++ Wallet/frontend/src/App.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { getWeb3, getWattet } from './utils.js';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccoutns] = useState(undefined);
   const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState('undefined');
+  const [quorum, setQuorum] = useState('undefined');
 
   useEffect(() => {
     const init = async () => {
       const web3 = getWeb3();
       const accounts = await web3.eth.getAccounts();
       const wallet = await getWallet(web3);
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.getQuorum().call();
       setWeb3(web3);
       setAccounts(accounts);
       setWallet(wallet);
+      setApprovers(approvers);
+      setQuorum(quorum);
     };
     init();
   }, []);
@@ -23,6 +30,8 @@ function App() {
     typeof web3 === 'undefined'
     || typeof accounts === 'undefined'
     || typeof wallet === 'undefined'
+    || typeof approvers === 'undefined'
+    || typeof quorum === 'undefined'
   ) {
     return <div> Loading...</div>
   }
@@ -30,6 +39,7 @@ function App() {
   return (
     <div>
       multisig
+      <Header approvers={approvers} quorum={quorum} />
     </div>
   );
 }
